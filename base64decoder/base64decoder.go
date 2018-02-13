c4dd10025e490810d8cccd16b260f274dd8d50db
Terry Harding <tharding@timbd.us>
2018-02-13 09:40:29 -0700

    first commit

--- base64decoder/base64decoder.go
+++ base64decoder/base64decoder.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-	if len(os.Args) < 2 {
+	if len(os.Args) != 2 {
 		fmt.Println("Usage:", "base64decoder <base64EncodedString>")
 	} else {
 		data, err := base64.StdEncoding.DecodeString(os.Args[1])
@@ -16,6 +16,6 @@ func main() {
 			fmt.Println("An error was encountered decoding supplied string.")
 			os.Exit(1)
 		}
-		fmt.Printf("%q\n", data)
+		fmt.Printf("%q", data)
 	}
 }
